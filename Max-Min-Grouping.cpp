ec9e6cd203e92413d5e7afaa22d9cb7b59aa3533
Harrison Lim Sun <91101408+harrisonlsun@users.noreply.github.com>
2022-10-29 00:08:47 -0400

    Add project files.

---
+++ Max-Min-Grouping.cpp
@@ -0,0 +1,12 @@
+﻿// Max-Min-Grouping.cpp : Defines the entry point for the application.
+//
+
+#include "Max-Min-Grouping.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
---
+++ Max-Min-Grouping.h
@@ -0,0 +1,8 @@
+﻿// Max-Min-Grouping.h : Include file for standard system include files,
+// or project specific include files.
+
+#pragma once
+
+#include <iostream>
+
+// TODO: Reference additional headers your program requires here.
